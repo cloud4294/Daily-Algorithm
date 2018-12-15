819666477ef869945c307f0949a4890b77dcf157
Kyung-Sun Lee <cloud4294@gmail.com>
2018-12-15 15:42:40 +0900

    2018-12-15

--- Main11724.java
+++ Main11724.java
@@ -1,81 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.StringTokenizer;
-
+import java.util.Scanner;
 
 /*
- * Baekjoon Online Judge 11724번 - 연결 요소의 개수
+ * 		Baekjoon Online Judge 11724번 - 연결요소의 개수
  * 
- * 문제 
- * 방향 없는 그래프가 주어졌을 때, 연결 요소 (Connected Component)의 개수를 구하는 프로그램을 작성하시오.
- * 
- * 입력 
- * 첫째 줄에 정점의 개수 N과 간선의 개수 M이 주어진다. (1 ≤ N ≤ 1,000, 0 ≤ M ≤ N×(N-1)/2) 둘째 줄부터 M개의 줄에 간선의 양 끝점 u와 v가 주어진다. (1 ≤ u, v ≤ N, u ≠ v) 같은 간선은 한 번만 주어진다.
- * 
- * 출력 
- * 첫째 줄에 연결 요소의 개수를 출력한다.
- * 
+ * 		https://www.acmicpc.net/problem/11724
  * 
  */
 
-public class Main11724 {
-	
-	static ArrayList<Integer>[] data;
-	static int checked[] ;
-	
-	
-	static int bfs(int i) {
-		
-		Queue<Integer> queue = new LinkedList<Integer>();
-		
-		queue.offer(i);
-		
-		while(!queue.isEmpty()) {
-			int current = queue.poll();
-			checked[current] = 1;
-			for (int j = 0; j < data[current].size(); j++) {
-				if(checked[data[current].get(j)] == 0)
-					queue.add(data[current].get(j));
-			}
-			
-		}
-		return 1;
-		
-	}
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		data = new ArrayList[N+1];
-		checked = new int[N+1];
+public class Main11724 {
+	static boolean[] check;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		boolean[] check = new boolean[N+1];
+		
+		List<Integer>[] list = new List[N+1];
+		
 		
 		for (int i = 1; i <= N; i++) {
-			data[i] = new ArrayList<Integer>();
+			list[i] = new ArrayList<Integer>();
 		}
+		
 		
 		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			int src = Integer.parseInt(st.nextToken());
-			int dst = Integer.parseInt(st.nextToken());
-			data[src].add(dst);
-			data[dst].add(src);
-		}
+			int src = sc.nextInt();
+			int dst = sc.nextInt();
+			
+			list[src].add(dst);
+			list[dst].add(src);
+		} // 그래프 구성
+		
 		int count = 0;
 		for (int i = 1; i <= N; i++) {
-			
-			if(checked[i] == 0) {
-				count += bfs(i);
+			if(check[i] == false) {
+				count++;
+				Queue<Integer> queue = new LinkedList<Integer>();
+				queue.offer(i);
+				check[i] = true;
+				
+				while(!queue.isEmpty()){
+					int now = queue.poll();
+					
+					for (int j = 0; j < list[now].size(); j++) {
+						int next = list[now].get(j);
+						if(check[next] == false) {
+							check[next] = true;
+							queue.offer(next);
+						}
+					}
+				}
+				
+				
 			}
-		}
-		
+		} // 방문하지 않은 모든 노드에 대해서 bfs를 시행, bfs시행 횟수를 출력
 		System.out.println(count);
 	}
-
 }

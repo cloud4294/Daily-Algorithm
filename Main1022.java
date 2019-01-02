7b0043ad74967cc35ed5c832bbc4670cf2d6f7c6
Kyung-Sun Lee <cloud4294@gmail.com>
2019-01-02 23:02:57 +0900

    2019-01-02

--- Main1022.java
+++ Main1022.java
@@ -1,99 +1,69 @@
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 /*
- *   Baekjoon Online Judge 1022번 - 소용돌이 예쁘게 출력하기 
+ * 		Baekjoon Online Judge 1022번 - 소용돌이 예브게 출력하기
  * 
- *   https://www.acmicpc.net/problem/1022
+ * 		https://www.acmicpc.net/problem/1022
+ * 
  */
 
 public class Main1022 {
 
-	static int[] mx = { 1, 0, -1, 0 };
-	static int[] my = { 0, -1, 0, 1 };
+	public static int solve(int i, int j) {
+
+		i = -i;
+		if (i > 0 && Math.abs(j) <= Math.abs(i) && i != -j) {  
+
+			return (Math.abs(i) * 2) * (Math.abs(i) * 2) - j - i + 1;
+		} else if (i <= 0 && Math.abs(j) <= Math.abs(i)) {
+
+			return (2 * Math.abs(i) + 1) * (2 * Math.abs(i) + 1) + (i + j);
+		} else if (j > 0 && i < Math.abs(j)) {
+
+			return (Math.abs(j - 1) + Math.abs(j)) * (Math.abs(j - 1) + Math.abs(j)) + i + j;
+		} else if (j < 0 && i <= Math.abs(j) && i != j) {
+
+			return (Math.abs(j) * 2) * (Math.abs(j) * 2) + 1 - (i + j);
+		}
+
+		return 0;
+
+	}// 규칙을 찾아 값을 리턴
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int r1 = Integer.parseInt(st.nextToken());
-		int c1 = Integer.parseInt(st.nextToken());
-		int r2 = Integer.parseInt(st.nextToken());
-		int c2 = Integer.parseInt(st.nextToken());
-		int max = 0;
-		if (Math.abs(r1) > max)
-			max = Math.abs(r1);
-		if (Math.abs(c1) > max)
-			max = Math.abs(c1);
-		if (Math.abs(r2) > max)
-			max = Math.abs(r2);
-		if (Math.abs(c2) > max)
-			max = Math.abs(c2); // 최대 크기를 찾는다 
 
-		int[][] map = new int[2 * max + 1][2 * max + 1]; // 최대크기로 맵사이즈를 정한다
-
-		int count = 1;
-		int x = max;
-		int y = max;
-		int length = 1;
-		map[max][max] = 1;
-
-		while (count < (2 * max + 1) * (2 * max + 1)) {
-			int check = 0;
-			for (int i = 0; i < 4; i++) {
-				check++;
-				for (int j = 0; j < length; j++) {
-					map[y][x] = count;
-					if (count == (2 * max + 1) * (2 * max + 1))
-						break;
-					x += mx[i];
-					y += my[i];
-					count++;
-				}
-				if (check == 2) {
-					length++;
-					check = 0;
-				} // 두번 방향을 꺾을때마다 길이가 증가한다 
-			}
-
-		}
-		int check = 0;
-		for (int i = max + r1; i <= max + r2; i++) {
-			for (int j = max + c1; j <= max + c2; j++) {
-
-				if (map[i][j] > check)
-					check = map[i][j];
-			}
-		}
-		int l = 0;
-		while (check > 0) {
-			check /= 10;
-			l++;
-		}// 출력값중 가장 긴값의 자리수를 구한다
-
-		for (int i = max + r1; i <= max + r2; i++) {
-			for (int j = max + c1; j <= max + c2; j++) {
-
-				String now = Integer.toString(map[i][j]);
-				if(j == max + c1) {
-					for (int k = 0; k < l - now.length(); k++) {
-						System.out.print(" ");
-					}
-				}else {
-					for (int k = 0; k <= l - now.length(); k++) {
-						System.out.print(" ");
-					}
-					
-				}
+		Scanner sc = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int r1 = sc.nextInt();
+		int c1 = sc.nextInt();
+		int r2 = sc.nextInt();
+		int c2 = sc.nextInt();
+		int size = Integer.max(
+				Integer.max(Integer.toString(solve(r1, c1)).length(), Integer.toString(solve(r1, c2)).length()),
+				Integer.max(Integer.toString(solve(r2, c1)).length(), Integer.toString(solve(r2, c2)).length()));
+		// 자리수를 맞추기 위해 가장긴 수를 찾아 길이를 저장 
+		
+		int length = 0;
+		for (int i = r1; i <= r2; i++) {
+			for (int j = c1; j <= c2; j++) {
+				int num = solve(i, j);
+				length = Integer.toString(num).length();
+				for (int k = 0; k < size - length; k++) {
+					bw.write(" ");
+				} // 부족한 자리수를 공백으로 채움
+				if(j != c1)
+					bw.write(" "); // 다음수와 구분하기 위한 공백
 				
-
-				System.out.print(now);
+				bw.write(Integer.toString(num));
 
 			}
-			System.out.println();
-		}// 출력조건에 맞게 출력한다.
+			bw.write("\n");
+			bw.flush();
+		}
+		bw.close();
 
 	}
 

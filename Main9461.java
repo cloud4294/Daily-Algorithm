d88104fbcbc6ccb9e5ad956a1b8a1586af1eacd8
Kyung-Sun Lee <cloud4294@gmail.com>
2019-01-08 11:02:35 +0900

    2019-01-08

---
+++ Main9461.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main9461 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		long[] dp = new long[101];
+		dp[1] = 1; dp[2] = 1; dp[3] = 1;
+		dp[4] = 2; dp[5] = 2;
+		for (int i = 6; i <= 100; i++) {
+			dp[i] = dp[i-1] + dp[i-5];
+		}
+		
+		while(T-->0) {
+			int N = Integer.parseInt(br.readLine());
+			System.out.println(dp[N]);
+		}
+
+	}
+
+}
